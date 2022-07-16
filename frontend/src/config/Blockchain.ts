7db623b7306c80616b3ccaf8191f2ddb66c89ea4
mpalczynski <pl.palczynski@gmail.com>
2022-07-16 13:59:58 +0200

    Changed provider type argument of helper method. Changed default ethereum test network name in env file.

--- frontend/src/config/Blockchain.ts
+++ frontend/src/config/Blockchain.ts
@@ -1,11 +1,12 @@
-import { ethers } from "ethers";
+import {ethers} from "ethers";
 import MESSAGE_ABI from "./Message.abi.json";
+import {Provider} from "@ethersproject/abstract-provider";
 
 // Provider is a class which provides an abstraction for a connection to the Ethereum Network
 // Alchemy Provider allows accessing Blockchain and its status without a personal wallet (read-only access)
 const alchemyProvider = new ethers.providers.AlchemyProvider(
-  process.env.REACT_APP_NETWORK,
-  process.env.REACT_APP_API_KEY
+    process.env.REACT_APP_NETWORK,
+    process.env.REACT_APP_API_KEY
 );
 
 // Web3Provider Provider uses browser plugin MetaMask which populates window.ethereum
@@ -13,11 +14,11 @@ const walletProvider = new ethers.providers.Web3Provider(window.ethereum);
 
 // Contract is an abstraction that represents a connection to a specific contract on the Ethereum Network,
 // so that applications can use it like a normal JavaScript object
-const getMessageContract = (provider: any) =>
-  new ethers.Contract(
-    process.env.REACT_APP_CONTRACT_ADDRESS,
-    MESSAGE_ABI.abi,
-    provider
-  );
+const getMessageContract = (provider: Provider) =>
+    new ethers.Contract(
+        process.env.REACT_APP_CONTRACT_ADDRESS,
+        MESSAGE_ABI.abi,
+        provider
+    );
 
-export { alchemyProvider, walletProvider, getMessageContract };
+export {alchemyProvider, walletProvider, getMessageContract};
